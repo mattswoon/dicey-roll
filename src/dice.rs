74199bf21171335b424b89aeba8dbe23ca8d00fd
Tom Watson <tom.watson@kinesis.org>
2020-09-15 08:26:43 +1000

    dice roller

--- src/dice.rs
+++ src/dice.rs
@@ -24,3 +24,58 @@ impl Dice {
         }
     }
 }
+
+impl std::fmt::Display for Dice {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Dice::D4 => write!(f, "d4"),
+            Dice::D6 => write!(f, "d6"),
+            Dice::D8 => write!(f, "d8"),
+            Dice::D10 => write!(f, "d10"),
+            Dice::D12 => write!(f, "d12"),
+            Dice::D20 => write!(f, "d20")
+        }
+    }
+}
+
+pub struct DiceTuple {
+    pub count: u32,
+    pub dice: Dice
+}
+
+impl std::str::FromStr for DiceTuple {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("d");
+        let num: u32 = split.next()
+            .ok_or(Error::ParseError(format!("Invalid dice string: {}", s)))?
+            .parse()
+            .unwrap_or(1);
+        let dice = match split.next() {
+            Some("4") => Ok(Dice::D4),
+            Some("6") => Ok(Dice::D6),
+            Some("8") => Ok(Dice::D8),
+            Some("10") => Ok(Dice::D10),
+            Some("12") => Ok(Dice::D12),
+            Some("20") => Ok(Dice::D20),
+            Some(d) => Err(Error::ParseError(format!("Invalid dice: {}", d))),
+            None => Err(Error::ParseError(format!("Invalid dice string: {}", s))),
+        }?;
+        Ok(DiceTuple { count: num, dice: dice })
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    ParseError(String)
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        dbg!(self);
+        match self {
+            Error::ParseError(s) => write!(f, "{}", s),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
+use std::str::FromStr;
 use serenity::{
     prelude::*,
-    model::{channel::Message, gateway::Ready},
+    model::channel::Message,
+    framework::standard::macros::{command, group},
+    framework::standard::{StandardFramework, CommandResult, CommandError},
 };
+use rand::thread_rng;
+use dicey_roll::dice::{DiceTuple};
+
+#[command]
+fn roll(ctx: &mut Context, msg: &Message) -> CommandResult {
+    println!("Gonna roll bby");
+    msg.react(&ctx, "👍")?;
+    let rng = thread_rng();
+    let rolls = msg.content.split(" ")
+        .skip(1)
+        .map(|s| DiceTuple::from_str(s.trim()))
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|e| CommandError(format!("{}", e)))?
+        .iter()
+        .map(|dt| { 
+            let rolls = (0..dt.count)
+                .map(|_| format!("{}", dt.dice.roll(rng)))
+                .collect::<Vec<_>>()
+                .join(", ");
+            format!("{} {} = {}", dt.count, dt.dice, rolls) })
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    msg.reply(&ctx, rolls)?;
+    Ok(())
+}
+
+#[group]
+#[commands(roll)]
+struct General;
 
 struct Handler;
 
-impl EventHandler for Handler {
-    fn message(&self, context: Context, msg: Message) {
-        match msg.content.split(" ").next() {
-            Some("!roll") => println!("Gonna roll some dice"),
-            _ => ()
-        }
-    }
-//
-//    fn ready(&self, _: Context, ready: Ready) {
-//        println!("{} is connected", reader.user.name);
-//    }
-}
+impl EventHandler for Handler {}
 
 fn main() {
     let token = std::env::var("DISCORD_TOKEN")
@@ -24,6 +46,10 @@ fn main() {
 
     let mut client = Client::new(&token, Handler)
         .expect("Couldn't create client");
+
+    client.with_framework(StandardFramework::new()
+                          .configure(|c| c.prefix("!"))
+                          .group(&GENERAL_GROUP));
 
     client.start().expect("Couldn't start client");
 }
